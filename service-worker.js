4884893a90efafd34ec51753956604d9a14b19b5
leliaoff <leliaoff@ya.ru>
2020-09-18 08:26:28 +0400

    Signed-off-by: leliaoff <leliaoff@ya.ru>

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2abb137b4ec37d2f642883ebd444b607.js"
+  "/precache-manifest.9d3aa432222b2cfa2333fd80957b598f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "umgorod.front"});
